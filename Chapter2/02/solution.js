7c2ec12cac1a34024bdbbff116e74cd5e5dd4c0b
tkdgns1341 <tkdgns1341@gmail.com>
2021-12-27 21:37:51 +0900

    2-1 2-2 누락 파일 커밋

--- Chapter2/02/solution.js
+++ Chapter2/02/solution.js
@@ -1,9 +1,18 @@
 // 문제 풀이
-function solution() {
-  let answer;
-
+function solution(inputValue) {
+  let input = `8
+  130 135 148 140 145 150 150 153`.split("\n");
+  input = input[1]
+    .trim()
+    .split(" ")
+    .slice(0, input[0] * 1)
+    .filter((value, index, arr) => value > arr[index - 1]);
+  let answer = input.length;
   return answer;
 }
 
 //출력
-console.log(solution);
+console.log(
+  solution(`8
+130 135 148 140 145 150 150 153`)
+);
