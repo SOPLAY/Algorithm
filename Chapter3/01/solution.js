6e61d4874ac200ad2a000426b3311390faadc717
tkdgns1341 <tkdgns1341@gmail.com>
2021-12-31 23:54:11 +0900

    3-1 Sol ADD

--- Chapter3/01/solution.js
+++ Chapter3/01/solution.js
@@ -1,9 +1,16 @@
 // 문제 풀이
 function solution(inputValue) {
-  let answer;
-
+  let answer = "YES";
+  inputValue = inputValue.toLowerCase();
+  let len = inputValue.length;
+  for (let i = 0; i < Math.floor(len / 2); i++) {
+    if (inputValue[i] !== inputValue[len - 1 - i]) {
+      answer = "NO";
+    }
+  }
+  console.log(inputValue);
   return answer;
 }
 
 //출력
-console.log(solution());
+console.log(solution(`gooG`));
