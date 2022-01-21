6c286ec0586ffa693dbc655efa9c94265b1c34dc
tkdgns1341 <tkdgns1341@gmail.com>
2022-01-21 23:13:01 +0900

    6-1 SOL ADD

--- Chapter6/01/solution.js
+++ Chapter6/01/solution.js
@@ -1,7 +1,17 @@
 // 문제 풀이
 function solution(inputValue) {
-  let input = inputValue.trim().split("\n");
-  let answer;
+  let input = inputValue.trim();
+  let answer = "YES";
+  let tempStack = [];
+  for (let str of input) {
+    if (str === "(") {
+      tempStack.push(str);
+    } else {
+      if (tempStack.length === 0) return "NO";
+      tempStack.pop();
+    }
+  }
+  if (tempStack.length > 0) return "NO";
   return answer;
 }
 
