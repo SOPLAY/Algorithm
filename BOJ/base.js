7cc01b27cfe4ec428f92468dea1fef08409a8f58
soplay <tkdgns1341@gmail.com>
2022-05-04 23:52:05 +0900

    백준 base 파일 수정

--- BOJ/base.js
+++ BOJ/base.js
@@ -1,4 +1,4 @@
 //백준 입력값 복붙
 let userInput = `백준 입력값`;
 // 백준서버에서 입력값 받아오는 부분    전처리 X
-let input = process.platform === "linux" ? require("fs").readFileSync("/dev/stdin") : userInput;
+let input = process.platform === "linux" ? require("fs").readFileSync("/dev/stdin").toString().trim() : userInput;
